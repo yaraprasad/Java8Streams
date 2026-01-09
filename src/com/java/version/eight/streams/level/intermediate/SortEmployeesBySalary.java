00d1390f508ad2aeb33368d5b0f2df0038a6b2c7
Prasad Yara <prasad.yara2779@gmail.com>
2026-01-09 11:15:26 +0530

    sort employees by salary

---
+++ src/com/java/version/eight/streams/level/intermediate/SortEmployeesBySalary.java
@@ -0,0 +1,30 @@
+package com.java.version.eight.streams.level.intermediate;
+
+import com.java.version.eight.streams.level.intermediate.dto.Employee;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
+public class SortEmployeesBySalary {
+    public static void main(String[] args) {
+    //Q. Sort*/ List of Employees By Salary
+        List<Employee> employees = Arrays.asList(
+                new Employee(101, "John", 50000),
+                new Employee(102, "Alice", 70000),
+                new Employee(103, "Bob", 45000),
+                new Employee(104, "David", 90000)
+        );
+
+        List<Employee> listAscending = employees.stream().sorted(Comparator.comparing(Employee::getSalary))
+                .toList();
+
+        System.out.println(listAscending);
+
+        List<Employee> listDescending = employees.stream().sorted(Comparator.comparing(Employee::getSalary).reversed())
+                .toList();
+
+        System.out.println(listDescending);
+
+    }
+}
