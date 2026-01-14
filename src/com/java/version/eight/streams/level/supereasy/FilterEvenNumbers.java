1b350adb6f72e039c2fd028586c82ef9dcd0d19f
Prasad Yara <prasad.yara2779@gmail.com>
2026-01-14 22:39:55 +0530

    FilterEvenNumbers class

---
+++ src/com/java/version/eight/streams/level/supereasy/FilterEvenNumbers.java
@@ -0,0 +1,22 @@
+package com.java.version.eight.streams.level.supereasy;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class FilterEvenNumbers {
+
+    public static void main(String[] args) {
+
+        // Q. Filter even numbers from list
+        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+        List<Integer> result = numbers.stream().filter(n -> n % 2 == 0).collect(Collectors.toList());
+        System.out.println(result);
+
+
+
+
+
+    }
+}
