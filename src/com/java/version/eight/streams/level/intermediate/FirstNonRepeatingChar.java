bfef8ff9c6c873671a31cf41b22743bfcd39fe69
Prasad Yara <prasad.yara2779@gmail.com>
2026-01-17 23:48:32 +0530

    FirstNonRepeatingChar class

---
+++ src/com/java/version/eight/streams/level/intermediate/FirstNonRepeatingChar.java
@@ -0,0 +1,23 @@
+package com.java.version.eight.streams.level.intermediate;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Optional;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+public class FirstNonRepeatingChar {
+    public static void main(String[] args) {
+    //Q. find first non repeating character in string
+        String input = "swiss";
+        Optional<Map.Entry<Character, Long>> frequentChars = input.chars()
+                .mapToObj(c -> (char) c)   //'s','w','i','s','s'
+                .collect(Collectors.groupingBy(Function.identity(), ////{s=3, w1, i=1}
+                        LinkedHashMap::new, Collectors.counting())) //preserve the insertion order
+                .entrySet().stream()
+                .filter(e -> e.getValue() == 1)
+                .findFirst();
+
+        System.out.println(frequentChars);
+    }
+}
