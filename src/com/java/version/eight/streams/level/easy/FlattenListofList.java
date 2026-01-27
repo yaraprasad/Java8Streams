f115509e9380a364b8746a36e3eb07d3b3ff33c8
Prasad Yara <prasad.yara2779@gmail.com>
2026-01-27 09:05:45 +0530

    FlattenListofList class

---
+++ src/com/java/version/eight/streams/level/easy/FlattenListofList.java
@@ -0,0 +1,22 @@
+package com.java.version.eight.streams.level.easy;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class FlattenListofList {
+    public static void main(String[] args) {
+    //Q. Flatten the list of list
+                List<List<Integer>> listOfList = Arrays.asList(
+                        Arrays.asList(1, 2, 3),
+                        Arrays.asList(4, 5),
+                        Arrays.asList(6, 7, 8, 9)
+                );
+                System.out.println(listOfList); //[[1, 2, 3], [4, 5], [6, 7, 8, 9]]
+
+                List<Integer> flatList = listOfList.stream()
+                        .flatMap(List::stream)
+                        .toList();
+                System.out.println(flatList); //[1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+    }
+}
